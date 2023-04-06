025499cde3e2ea4b2ead18c6d0d76e20e22a553c
achrafmejd <achraf.mejd@ensam-casa.ma>
2023-04-06 11:12:06 +0000

    Smartfilterbar added

--- webapp/controller/VendorsMain.controller.js
+++ webapp/controller/VendorsMain.controller.js
@@ -11,17 +11,17 @@ sap.ui.define([
         return Controller.extend("vendors.controller.VendorsMain", {
             onInit: function () {
                 // Set static values for GenericTile
-                var oGenericTile = this.getView().byId("_IDGenGenericTile2");
-                oGenericTile.setHeader("Achraf");
-                oGenericTile.setSubheader("a RadialMicroChart");
-                oGenericTile.setFrameType("OneByOne");
+                // var oGenericTile = this.getView().byId("_IDGenGenericTile2");
+                // oGenericTile.setHeader("Achraf");
+                // oGenericTile.setSubheader("a RadialMicroChart");
+                // oGenericTile.setFrameType("OneByOne");
 
-                // Set static values for RadialMicroChart
-                var oRadialMicroChart = this.getView().byId("_IDGenRadialMicroChart3");
-                oRadialMicroChart.setSize("Responsive");
-                oRadialMicroChart.setPercentage(99.9);
-                oRadialMicroChart.setAlignContent("Right");
-                // oRadialMicroChart.setValueColor(RadialMicroChart.ValueColor.Neutral);
+                // // Set static values for RadialMicroChart
+                // var oRadialMicroChart = this.getView().byId("_IDGenRadialMicroChart3");
+                // oRadialMicroChart.setSize("Responsive");
+                // oRadialMicroChart.setPercentage(99.9);
+                // oRadialMicroChart.setAlignContent("Right");
+                // // oRadialMicroChart.setValueColor(RadialMicroChart.ValueColor.Neutral);
             }
         });
     });
