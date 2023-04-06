4ce0110f623e1fa814d9461578b313b06f44e9ed
achrafmejd <achraf.mejd@ensam-casa.ma>
2023-04-06 18:27:11 +0000

    All 3 charts added and working

--- webapp/controller/VendorsMain.controller.js
+++ webapp/controller/VendorsMain.controller.js
@@ -11,11 +11,11 @@ sap.ui.define([
         "use strict";
 
         return Controller.extend("vendors.controller.VendorsMain", {
-            _onGetPercentageOfMoroccanVendors(myData) {
+            _onGetPercentage(myData, field, value) {
                 // Calculate the sum
                 let sum = 0
                 myData.forEach((item) => {
-                    if (item.Land1 == 'MA') {
+                    if (item[field] == value) {
                         sum += 1
                     }
                 })
@@ -41,26 +41,26 @@ sap.ui.define([
                             console.log(data);
                             if (data.results) {
                                 // Destructing the Objects to get only used fields
-                                const oData = data.results.map(({ Lifnr, Land1, Name1, Stras, Ort01, Pstlz, Telf1, Stcd1 }) => ({ Lifnr, Land1, Name1, Stras, Ort01, Pstlz, Telf1, Stcd1 }))
+                                const oData = data.results.map(({ Lifnr, Land1, Name1, Stras, Ort01, Pstlz, Telf1, Stcd1, Spras, Stkzn}) => ({ Lifnr, Land1, Name1, Stras, Ort01, Pstlz, Telf1, Stcd1, Spras, Stkzn }))
                                 console.log(oData);
                                 // Create the JSON model
                                 const jModel = new sap.ui.model.json.JSONModel(oData);
                                 // Bind the model to the view
-                                that.getView().byId("table").setModel(jModel);
-                                // Calculate the Percentage for MA Code - Start 
-
-                                // Set static values for GenericTile
-                                var oGenericTile = that.getView().byId("_IDGenGenericTile1");
-                                oGenericTile.setHeader("Fournisseur");
-                                oGenericTile.setSubheader("au Maroc");
-                                oGenericTile.setFrameType("OneByOne");
-
-                                // Set static values for RadialMicroChart
+                                that.getView().byId("table").setModel(jModel);        
+                                /* ##################"" START - Setting DONUT VALUES - #################### */
+                                // Set static values for RadialMicroChart ---> First one 
                                 var oRadialMicroChart = that.getView().byId("_IDGenRadialMicroChart1");
-                                oRadialMicroChart.setSize("Responsive");
-                                oRadialMicroChart.setPercentage(that._onGetPercentageOfMoroccanVendors(oData));
-                                oRadialMicroChart.setAlignContent("Right");
-                                // oRadialMicroChart.setValueColor(RadialMicroChart.ValueColor.Neutral);
+                                // Calculate and set valus to the view
+                                oRadialMicroChart.setPercentage(that._onGetPercentage(oData, 'Land1', 'MA'));
+                                // Set static values for RadialMicroChart ---> Second one 
+                                var oRadialMicroChart = that.getView().byId("_IDGenRadialMicroChart2");
+                                // Calculate and set valus to the view
+                                oRadialMicroChart.setPercentage(that._onGetPercentage(oData, 'Spras', 'FR'));
+                                // Set static values for RadialMicroChart ---> Third one 
+                                var oRadialMicroChart = that.getView().byId("_IDGenRadialMicroChart3");
+                                // Calculate and set valus to the view
+                                oRadialMicroChart.setPercentage(that._onGetPercentage(oData, 'Stkzn', 'X'));
+                                /* ##################"" END - Setting DONUT VALUES - #################### */
 
 
 
