ca4c788f61ebea95c7f5d2bbf3c4a1a94e8fe183
achrafmejd <achraf.mejd@ensam-casa.ma>
2023-04-11 13:13:12 +0000

    Viz chart added to single object page

--- webapp/controller/Vendor.controller.js
+++ webapp/controller/Vendor.controller.js
@@ -1,28 +1,61 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/core/routing/History"
+    "sap/ui/core/routing/History",
+    'sap/viz/ui5/format/ChartFormatter',
+
 
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, History) {
+    function (Controller, History, ChartFormatter) {
         "use strict";
 
         return Controller.extend("vendors.controller.Vendor", {
-            onInit: function() {
+            onInit: function () {
                 const oRouter = sap.ui.core.UIComponent.getRouterFor(this); // Get the router instance
                 oRouter.getRoute("RouteVendorSingleObjectPage").attachPatternMatched(this._onRouteMatched, this); // Attach a pattern matched event handler
-              },
-            _onRouteMatched: function(oEvent) {
-                const oSelectedItem = oEvent.getParameter("arguments").id; // Access the passed parameter from the first view
-                console.log("HELLO FROM THE SECOND VIE<");
-                console.log(JSON.parse(oSelectedItem));
+                const data = {
+                    myData: [
+                        {
+                            "Commande": "Validée",
+                            "Nombre": 50
+                        },
+                        {
+                            "Commande": "Non Validée",
+                            "Nombre": 40
+                        },
+                        {
+                            "Commande": "Supprimée",
+                            "Nombre": 10
+                        }
+                    ]
+                }
+                var jsonData = new sap.ui.model.json.JSONModel(data);
+                var oVizFrame = this.getView().byId("idVizFrame");
+                if(oVizFrame){
+                    oVizFrame.setModel(jsonData);
+                    oVizFrame.setVizProperties({
+                        plotArea: {
+                            dataLabel: {
+                                visible: true
+                            }
+                        }
+                    });
+                }
+    
+                var oPopOver = this.getView().byId("idPopOver");
+                oPopOver.connect(oVizFrame.getVizUid());
+                oPopOver.setFormatString(ChartFormatter.DefaultPattern.STANDARDFLOAT);
             },
-            onNavBack: function(){
+            _onRouteMatched: function (oEvent) {
+                // Access the passed parameter from the first view
+                const oSelectedItem = oEvent.getParameter("arguments").id; 
+            },
+            onNavBack: function () {
                 var oHistory = History.getInstance();
                 var sPreviousHash = oHistory.getPreviousHash();
-    
+
                 if (sPreviousHash !== undefined) {
                     window.history.go(-1);
                 } else {
